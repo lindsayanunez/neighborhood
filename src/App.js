b6b6059abe07161b3d44a9df85fcf1151252f3ef
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-11 22:05:04 -0600

    Updated imports and added 'Display Map' componenet to the render.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import locations from '.data/loctions.json';
+import DisplayMap from '.components/DisplayMap';
 
 class App extends Component {
+  state = {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>'Best Pizza in Brooklyn'</h1>
+      </div>
+      <DisplayMap/>
+
       </div>
     );
   }
