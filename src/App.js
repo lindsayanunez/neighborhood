b4f482445ec59415be861693e13b4c0fe85c42e1
Lindsay Nunez <lindsayanunez@gmail.com>
2018-11-13 20:39:18 -0600

    Added the list drawer component and the button to render.

--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import locations from './data/locations.json';
 import DisplayMap from './components/DisplayMap';
+import ListDrawer from './components/ListDrawer';
 
 class App extends Component {
   state = {
     lat: 40.6782,
     lon: -73.9442,
     zoom: 15,
-    all: locations
+    all: locations,
+    open: false
+  }
+
+  toggleDrawer = () => {
+    //change state to show if drawer is open or not
+    this.setState({
+      open: !this.state.open
+    });
   }
 
   render = () => {
     return (
       <div className="App">
       <div>
+        <button onClick={this.toggleDrawer} style={this.styles.menuButton>
+          <i className="fa fa-bars">
+        </button>
         <h1>'Best Pizza in Brooklyn'</h1>
       </div>
         <DisplayMap
@@ -22,7 +34,10 @@ class App extends Component {
           lon={this.state.lon}
           zoom={this.state.zoom}
           locations={this.state.all}/>
-
+        <ListDrawer
+          locations={this.state.all}
+          open={this.state.open}
+          toggleDrawer={this.toggleDrawer}/>
       </div>
     );
   }
