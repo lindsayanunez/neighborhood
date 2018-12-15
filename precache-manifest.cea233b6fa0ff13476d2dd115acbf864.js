2a90bb3e1430e64b135d8dc752195c90f445587c
Lindsay Nunez <lindsayanunez@gmail.com>
2018-12-15 13:39:15 -0500

    Updates

---
+++ precache-manifest.cea233b6fa0ff13476d2dd115acbf864.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08962b4d5f2d1ad2b5bc",
+    "url": "/neighborhood/static/js/runtime~main.08962b4d.js"
+  },
+  {
+    "revision": "14585bc5c6a809d346dc",
+    "url": "/neighborhood/static/js/main.14585bc5.chunk.js"
+  },
+  {
+    "revision": "3816d1b12144957388ed",
+    "url": "/neighborhood/static/js/1.3816d1b1.chunk.js"
+  },
+  {
+    "revision": "14585bc5c6a809d346dc",
+    "url": "/neighborhood/static/css/main.ea1bd751.chunk.css"
+  },
+  {
+    "revision": "eec72c272331fe57d8f0272246147eb0",
+    "url": "/neighborhood/index.html"
+  }
+];
